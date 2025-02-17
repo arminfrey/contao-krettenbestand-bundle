d0d2676f70cc4f5fcede7950081427906d2bbcfd
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-02-17 15:13:18 +0100

    Create default.php

---
+++ contao/languages/en/default.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-krettenbestand-bundle.
+ *
+ * (c) Armin Frey 2025 <webmaster@krettenweiber.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/arminfrey/contao-krettenbestand-bundle
+ */
+
+/**
+ * Miscellaneous
+ */
+//$GLOBALS['TL_LANG']['MSC'][''] = '';
+
+/**
+ * Errors
+ */
+//$GLOBALS['TL_LANG']['ERR'][''] = '';
