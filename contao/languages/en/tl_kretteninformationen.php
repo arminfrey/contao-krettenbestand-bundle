b76cb07f6ce2291629c26c3508ae5aeffb22e7ed
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2025-02-17 15:10:50 +0100

    Create tl_kretteninformationen.php

---
+++ contao/languages/en/tl_kretteninformationen.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-krettenbestand-bundle.
+ *
+ * (c) Armin Frey 2025 <webmaster@krettenweiber.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/arminfrey/contao-krettenbestand-bundle
+ */
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['first_legend'] = "Basis Einstellungen";
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['second_legend'] = "Erweiterte Einstellungen";
+
+/**
+* Global operations
+*/
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['new'] = ["Neu", "Ein neues Element anlegen"];
+
+/**
+ * Operations
+ */
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['edit'] = "Datensatz mit ID: %s bearbeiten";
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['copy'] = "Datensatz mit ID: %s kopieren";
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['delete'] = "Datensatz mit ID: %s löschen";
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['show'] = "Datensatz mit ID: %s ansehen";
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['title'] = ["Titel", "Geben Sie den Titel ein"];
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['selectField'] = ["Select Feld", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['checkboxField'] = ["Chosen Feld", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['multitextField'] = ["Multitext Feld", "Geben Sie die Werte ein"];
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['addSubpalette'] = ["Erweiterte Einstellungen aktivieren", "Hier können Sie die erweiterten Einstellungen aktivieren."];
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['textareaField'] = ["Textarea", "Geben Sie einen Text ein"];
+
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['firstoption'] = "Erste Option";
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['secondoption'] = "Zweite Option";
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_kretteninformationen']['customButton'] = "Custom Routine starten";
